8693eb3bb38267b4405689eb6389e47eebd52d6a
mdu74 <mdu74.C@gmail.com>
2017-08-08 10:21:45 +0200

    Javascript tests

---
+++ BalancedBracketsKata.Tests/spec/balancedBrackets.spec.js
@@ -0,0 +1,44 @@
+﻿"use strict";
+
+describe("BalancedBrackets", function () {
+    it("Given empty string should return OK", function () {
+        //---------------Set up test pack-------------------
+        var input = '';
+        //---------------Execute Test ----------------------
+        var result = BalancedBrackets(input);
+        //---------------Test Result -----------------------
+        expect(result).toBe("OK");
+    });
+    it("Given open bracket should return FAIL", function () {
+        //---------------Set up test pack-------------------
+        var input = '[';
+        //---------------Execute Test ----------------------
+        var result = BalancedBrackets(input);
+        //---------------Test Result -----------------------
+        expect(result).toBe("FAIL");
+    });
+    it("Given closed bracket should return FAIL", function () {
+        //---------------Set up test pack-------------------
+        var input = ']';
+        //---------------Execute Test ----------------------
+        var result = BalancedBrackets(input);
+        //---------------Execute Test ----------------------
+        expect(result).toBe("FAIL");
+    });
+    it("Given open and close bracket and should return OK", function () {
+        //---------------Set up test pack-------------------
+        var input = '[]';
+        //---------------Execute Test ----------------------
+        var result = BalancedBrackets(input);
+        //---------------Execute Test ----------------------
+        expect(result).toBe("OK");
+    });
+    it("Given unbalanced brackets and should return FAIL", function () {
+        //---------------Set up test pack-------------------
+        var input = '[][';
+        //---------------Execute Test ----------------------
+        var result = BalancedBrackets(input);
+        //---------------Execute Test ----------------------
+        expect(result).toBe("FAIL");
+    });
+});
---
+++ BalancedBracketsKata/js/balancedBrackets.js
@@ -0,0 +1,27 @@
+﻿"use strict";
+
+function BalancedBrackets(input) {
+    var bracketBalanceCount = 0;
+    var arrayOfBrackets = Array.from(input);
+    arrayOfBrackets.forEach(function (bracket) {
+        if (CheckForOpenBracket(bracket))
+            bracketBalanceCount++;
+        else
+            bracketBalanceCount--;
+        if (CheckIfBracketBalanceCount(bracketBalanceCount))
+            return 'FAIL';
+    });
+    return CheckIfBracketBalanceCountIsZero(bracketBalanceCount);
+}
+
+function CheckForOpenBracket(bracket) {
+    return bracket === '[';
+}
+
+function CheckIfBracketBalanceCountIsZero(bracketBalanceCount) {
+    return bracketBalanceCount === 0 ? 'OK' : 'FAIL';
+}
+
+function CheckIfBracketBalanceCount(bracketBalanceCount) {
+    return bracketBalanceCount < 0;
+}
